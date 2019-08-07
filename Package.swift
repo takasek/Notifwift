73c3191f66d80d96ca71a8d492441d1c7ec0b35e
TAKEDA hiroyuki <h_takeda@waku-2.com>
2019-08-07 16:22:27 +0900

    Upgrade swift tool version for SPM

--- Package.swift
+++ Package.swift
@@ -1,5 +1,16 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
-    name: "Notifwift"
+    name: "Notifwift",
+    platforms: [
+        .iOS(.v8)
+    ],
+    products: [
+        .library(name: "Notifwift", targets: ["Notifwift"]),
+    ],
+    targets: [
+        .target(name: "Notifwift", path: "Notifwift"),
+    ]
 )
