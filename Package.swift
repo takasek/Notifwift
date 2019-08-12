648bf8cc90c2b8c1e9f6712bb4b315419a308b2e
Yoshitaka Seki <takassekiyoshi@gmail.com>
2019-08-12 17:58:57 +0900

    Modify Package.swift for Swift 4.2

--- Package.swift
+++ Package.swift
@@ -1,12 +1,9 @@
-// swift-tools-version:5.1
+// swift-tools-version:4.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
     name: "Notifwift",
-    platforms: [
-        .iOS(.v8)
-    ],
     products: [
         .library(name: "Notifwift", targets: ["Notifwift"]),
     ],
